c6e1eba9ee50297321ca15f88fa79a0f3dde9d2a
Zach van Noppen <44442526+ZachvanNoppen@users.noreply.github.com>
2020-01-09 00:33:34 -0500

    Updates

---
+++ precache-manifest.aa7baac385b427d41fc6ca55940ebefe.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cd9135bf28b09c2e3cdce9b43548d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32f9069814e875fa275b",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "b6ca49aa2af47c479170",
+    "url": "/static/css/main.e1787789.chunk.css"
+  },
+  {
+    "revision": "32f9069814e875fa275b",
+    "url": "/static/js/2.316a0b5f.chunk.js"
+  },
+  {
+    "revision": "326b0a0c3a30fb73841a78d396b72a6f",
+    "url": "/static/js/2.316a0b5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6ca49aa2af47c479170",
+    "url": "/static/js/main.b6ec8a16.chunk.js"
+  },
+  {
+    "revision": "6cc25e9bb97f413f440e",
+    "url": "/static/js/runtime-main.1fc005ba.js"
+  },
+  {
+    "revision": "38dadb3692fbcba34dd3b6351b7e89d7",
+    "url": "/static/media/Alone.38dadb36.PNG"
+  },
+  {
+    "revision": "5490ad4cc59b93377b023495e01382a7",
+    "url": "/static/media/handlebar.5490ad4c.PNG"
+  },
+  {
+    "revision": "74e3e8fd23eb881cbae4b2d72c13d6b3",
+    "url": "/static/media/hide_seek.74e3e8fd.PNG"
+  },
+  {
+    "revision": "cfe7b58a0b58a830a5fcfcd4f083d444",
+    "url": "/static/media/hospital_database.cfe7b58a.PNG"
+  },
+  {
+    "revision": "a837ec19041462dbdcb132e2168a8a95",
+    "url": "/static/media/leap_flight.a837ec19.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "136d860e908284476d971158ecb7a2ea",
+    "url": "/static/media/me.136d860e.PNG"
+  },
+  {
+    "revision": "3c9d5c9aba5b1bb51d9fec056636b4a4",
+    "url": "/static/media/personal.3c9d5c9a.png"
+  },
+  {
+    "revision": "d485080f41df014ca12ffa0696cf70f6",
+    "url": "/static/media/seeing_sound.d485080f.PNG"
+  },
+  {
+    "revision": "6f1336ca49941b24601910ec8e107285",
+    "url": "/static/media/sound_visualiser.6f1336ca.PNG"
+  },
+  {
+    "revision": "940ca51ee94c478226371cfe93907eae",
+    "url": "/static/media/vaat.940ca51e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site/precache-manifest.3f129fa06a1df00441299b82f32a3a90.js"
+  "/precache-manifest.aa7baac385b427d41fc6ca55940ebefe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
